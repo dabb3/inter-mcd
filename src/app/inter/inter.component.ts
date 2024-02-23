46dbf19f2caaa78054b7d3f63f386ec1bb4ec2ac
Silva Cunha Lucas Caio (Contractor) <lucas.i.silva@capgemini.com>
2024-02-23 11:46:35 -0300

    commit
    commit

--- src/app/inter/inter.component.ts
+++ src/app/inter/inter.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import moment from 'moment-timezone';
 
 @Component({
   selector: 'app-inter',
@@ -8,14 +9,38 @@ import { TranslateService } from '@ngx-translate/core';
 })
 
 export class InterComponent {
+  currentTime: string;
+  clockInterval: any;
+  name: string;
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
+    this.startClock('America/New_York');
+    this.currentTime = '';
+    this.name = '';
   }
 
   switchLanguage(language: string) {
     this.translate.use(language);
+    if (language === 'en') {
+      this.startClock('America/New_York');
+    } else if (language === 'fr') {
+      this.startClock('Europe/Paris');
+    } else if (language === 'de') {
+      this.startClock('Europe/Berlin');
+    }
   }
 
-  
+  getCurrentTime(timezone: string) {
+    return moment().tz(timezone).format('HH:mm:ss');
+  }
+
+  startClock(timezone: string) {
+    if (this.clockInterval) {
+      clearInterval(this.clockInterval);
+    }
+    this.clockInterval = setInterval(() => {
+      this.currentTime = this.getCurrentTime(timezone);
+    }, 1000);
+  }
 }
